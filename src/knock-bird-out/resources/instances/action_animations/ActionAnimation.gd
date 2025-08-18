0664a5d34ea5d01ac7db8c33d392dfac81041ec5
dolloreanadog <dolloreanaseabass@gmail.com>
2025-08-18 16:53:07 -0700

    Create new ActionAnimations

--- src/knock-bird-out/resources/instances/action_animations/ActionAnimation.gd
+++ src/knock-bird-out/resources/instances/action_animations/ActionAnimation.gd
@@ -3,8 +3,14 @@ class_name ActionAnimation
 
 @onready var animationPlayer: AnimationPlayer = $AnimationPlayer
 
+## When the animation finishes, this will emit
 signal ActionFinished
 
+func Debug() -> void:
+	print(animationPlayer.current_animation)
+
+## Deletes and emits ActionFinished Signal
 func Remove() -> void:
 	ActionFinished.emit()
+	print("Play Animation")
 	queue_free()
